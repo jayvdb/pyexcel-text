ab74714196156625b18b3eaa4aad97921a360a40
John Vandenberg <jayvdb@gmail.com>
2016-04-03 05:25:48 +0700

    Test missing column

--- tests/test_io.py
+++ tests/test_io.py
@@ -214,7 +214,8 @@ class TestIO(unittest.TestCase):
             ["Column 1", "Column 2", "Column 3"],
             [1, 2, 3],
             [4, 5, 6],
-            [7, 8]
+            [7, 8],
+            [9, 10, 11],
         ]
         s = pe.Sheet(content, name_columns_by_row=0)
 
